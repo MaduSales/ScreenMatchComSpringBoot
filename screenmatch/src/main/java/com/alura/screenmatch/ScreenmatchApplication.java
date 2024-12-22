0bc687bacbe64c3fa21ce2994f4e0f11af229455
Maria Eduarda <maria.eduarda15miranda@gmail.com>
2024-12-22 20:18:45 -0300

    desserialização de Json com Jackson

---
+++ screenmatch/src/main/java/com/alura/screenmatch/ScreenmatchApplication.java
@@ -0,0 +1,27 @@
+package com.alura.screenmatch;
+
+import com.alura.screenmatch.model.DadosSerie;
+import com.alura.screenmatch.service.ConsumoApi;
+import com.alura.screenmatch.service.ConverteDados;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ScreenmatchApplication implements CommandLineRunner {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ScreenmatchApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		var consumoApi = new ConsumoApi();
+		var json = consumoApi.obterDados("https://www.omdbapi.com/?t=gilmore+girls&apikey=17d728ce");
+		System.out.println(json);
+		ConverteDados conversor = new ConverteDados();
+		DadosSerie dados = conversor.obterDados(json,DadosSerie.class);
+		System.out.println(dados);
+
+	}
+}
